4c1afcbb120b2b31df3dac9aab55600101a0e460
Wayne Major <wayne.major03@gmail.com>
2020-04-28 15:44:34 -0400

    Change -notlike to -CNotMatch

--- powershell/CleanUpFilePathAndWhiteListFolders.ps1
+++ powershell/CleanUpFilePathAndWhiteListFolders.ps1
@@ -1,5 +1,5 @@
-$limit = (Get-Date).AddDays(-3)
+$limit = (Get-Date).AddDays(-2)
 $path = "SomePath"
 
 #Deletes files older than X Days from Path and whitelist folders and files that can't be touched.  
-Get-ChildItem -Path $path -Force -Recurse | Where-Object {!_.PSIsContainer -and $_.CreationTime -lt $limit} | ?{$_.FullName -notlike "test.*|.test2*"} | Remove-Item -Force
+Get-ChildItem -Path $path -Force -Recurse | Where-Object {!_.PSIsContainer -and $_.CreationTime -lt $limit} | ?{$_.FullName -CNotmatch "test.*|.test2*"} | Remove-Item -Force
