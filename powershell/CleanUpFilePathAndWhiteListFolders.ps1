0804670848db722db2ab09615fcaff5ff1577e5f
Wayne Major <wayne.major03@gmail.com>
2020-05-01 20:16:08 -0400

    Switch position of wildcard character.

--- powershell/CleanUpFilePathAndWhiteListFolders.ps1
+++ powershell/CleanUpFilePathAndWhiteListFolders.ps1
@@ -2,4 +2,4 @@ $limit = (Get-Date).AddDays(-2)
 $path = "SomePath"
 
 #Deletes files older than X Days from Path and whitelist folders and files that can't be touched.  
-Get-ChildItem -Path $path -Force -Recurse | Where-Object {!_.PSIsContainer -and $_.CreationTime -lt $limit} | ?{$_.FullName -CNotmatch "test.*|.test2*"} | Remove-Item -Force
+Get-ChildItem -Path $path -Force -Recurse | Where-Object {!_.PSIsContainer -and $_.CreationTime -lt $limit} | ?{$_.FullName -CNotmatch "test.*|test2.*"} | Remove-Item -Force
