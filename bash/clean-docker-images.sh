3b001b9f0a0ddcc82720341479db496ed56d2ddb
Wayne Major <wayne.major03@gmail.com>
2021-07-22 10:31:38 -0400

    Update clean-docker-images.sh
    Added an RMI remove command as well.

--- bash/clean-docker-images.sh
+++ bash/clean-docker-images.sh
@@ -2,3 +2,4 @@
 
 # Clean docker images older than 7 days in docker build vm.
 docker image prune -a -f --filter "until=$(date +'%Y-%m-%dT%H:%M:%S' --date='-15 days')"
+docker rmi -f `docker images | grep '<none>' | awk '{print $3}'`
